0efdd1b2e1adc21dfdcdeabde10a0d29770941ed
Tomás Alberto Mendoza Infanzón <albertomendozainf@gmail.com>
2024-05-06 13:53:32 +0200

    vars

--- astro.config.mjs
+++ astro.config.mjs
@@ -23,6 +23,10 @@ export default defineConfig({
     ssr: {
       target: "node",
       external: ["firebase-admin"]
+    },
+    define: {
+      'process.env.TURSO_DATABASE_URL': JSON.stringify(process.env.TURSO_DATABASE_URL),
+      'process.env.TURSO_AUTH_TOKEN': JSON.stringify(process.env.TURSO_AUTH_TOKEN)
     }
   }
 });
--- src/turso.ts
+++ src/turso.ts
@@ -2,17 +2,7 @@ export const prerender = false;
 
 import { createClient } from "@libsql/client/web";
 
-const url = import.meta.env.TURSO_DATABASE_URL?.trim();
-if (url === undefined) {
-  throw new Error("TURSO_URL env var is not defined");
-}
-
-const authToken = import.meta.env.TURSO_AUTH_TOKEN?.trim();
-if (authToken == undefined) {
-  throw new Error("TURSO_AUTH_TOKEN env var is not defined");
-}
-
 export const turso = createClient({
-  url: url,
-  authToken: authToken,
+  url: import.meta.env.TURSO_DATABASE_URL,
+  authToken: import.meta.env.TURSO_AUTH_TOKEN,
 });
