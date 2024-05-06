94fbe6f30b0e2ddc2028b003a7664e18e54faa9e
Tomás Alberto Mendoza Infanzón <albertomendozainf@gmail.com>
2024-05-06 12:50:59 +0200

    api get

--- src/pages/api/getchat.json.ts
+++ src/pages/api/getchat.json.ts
@@ -4,7 +4,12 @@ import type { APIRoute } from "astro";
 import { turso } from "@src/turso";
 
 export const GET: APIRoute = async () => {
-  const { rows } = await turso.execute("SELECT nombre, mensaje, fecha FROM chat ORDER BY fecha DESC LIMIT 20");
-  return new Response(JSON.stringify(rows));
+  try {
+    const { rows } = await turso.execute("SELECT nombre, mensaje, fecha FROM chat ORDER BY fecha DESC LIMIT 20");
+    return new Response(JSON.stringify(rows));
+  } catch (error) {
+    console.error(error);
+    return new Response('Error al ejecutar la consulta', { status: 500 });
+  }
 }
 
